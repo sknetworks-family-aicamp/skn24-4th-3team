79d79fdfb28937c0f74e5e7914f0797d8867469d
swimmingpool <suyoung6279@gmail.com>
2026-05-04 11:04:43 +0900

    no cache

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'config.middleware.NoCacheAuthenticatedPagesMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
